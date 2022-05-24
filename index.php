161a0195505da66d410961e1a34b9058961a5877
Alexander <formysite.ru@yandex.ru>
2022-05-24 13:26:43 +0300

    first

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MySQL и PHPMYADMIN</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+</head>
+<body>
+    <main>
+        <header>
+            <h1>MySQL и PHPMYADMIN.<br> 
+                Начало работы</h1>
+                <h4>Знакомимся с системой администрирования <b>PHPMyAdmin</b> и создадим sql-таблицу для сайта</h4>
+                    <?php
+                        $nickname="Александр";
+                        $hello="Привет, ";
+                        $offer="Продолжаем изучение кода PHP и в данном параграфе рассмотрим работу с базой данных(БД) в MySQL с панелью управлени PHPMyAdmin.";
+                        echo "<h2>".$hello.$nickname."<br>".$offer."</h2>";
+
+                    ?>
+        
+        </header>
+        
+    </main>
+    
+</body>
+</html>
